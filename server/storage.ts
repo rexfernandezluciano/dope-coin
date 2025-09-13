9ab2b552a9241df65bc9d669ce94414e7452ee6b
Rex Luciano <rexluciano@yahoo.com>
2025-09-13 23:48:50 +0000

    Fix schema imports

--- server/storage.ts
+++ server/storage.ts
@@ -13,7 +13,7 @@ import {
   type Wallet,
   type InsertWallet,
   type NetworkStats,
-} from "@shared/schema.js";
+} from "../shared/schema.js";
 import { db } from "./db.js";
 import { eq, and, desc, sum, count, sql, gte } from "drizzle-orm";
 
